b91e5038eadf1014698db0a5676c7237f8861b8f
privas <rivaslpa@gmail.com>
2025-06-21 21:26:15 -0400

    Added Insertion Sort

--- script.js
+++ script.js
@@ -34,11 +34,27 @@ async function bubbleSort() {
                 var temp = values[j]
                 values[j] = values[j + 1]
                 values[j + 1] = temp
-                await sleep(20);
 
+                await sleep(20);
                 renderValues();
             }
         }
+    }
+}
+
+async function insertionSort() {
+    for (let i = 1; i < values.length; i++) {
+        let key = values[i];
+        let j = i - 1;
+
+        while (j >= 0 && values[j] > key) {
+            values[j + 1] = values[j];
+            j = j - 1;
+        }
+        values[j + 1] = key;
+
+        await sleep(20);
+        renderValues();
     }
 }
 
@@ -56,6 +72,11 @@ document.getElementById("randomizeButton").onclick = function(){
 
 document.getElementById("bubbleSortButton").onclick = function(){
     bubbleSort();
+    renderValues();
+}
+
+document.getElementById("insertionSortButton").onclick = function(){
+    insertionSort();
     renderValues();
 }
 
